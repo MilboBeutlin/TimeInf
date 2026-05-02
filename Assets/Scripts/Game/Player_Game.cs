2b5a078212d59de15d5c6d7bcd2cf870861e33fa
DarthShifter <darthstein66@gmail.com>
2026-05-02 16:56:10 +0200

    Wand+Playermovement adjustment

--- Assets/Scripts/Game/Player_Game.cs
+++ Assets/Scripts/Game/Player_Game.cs
@@ -3,54 +3,59 @@ using UnityEngine.InputSystem;
 
 public class Player_Game : MonoBehaviour
 {
-    
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private Rigidbody2D body;
-    [SerializeField] private Sprite[] sprites;
-    [SerializeField] private SpriteRenderer sR;
-    private float x;
-    private float y;
-    private Vector2 move;
-    void Start()
-    {
-        
-    }
+[SerializeField] private float speed = 5f;
+[SerializeField] private Rigidbody2D body;
+[SerializeField] private LayerMask wallLayer;
+[SerializeField] private Sprite[] sprites;
+[SerializeField] private SpriteRenderer sR;
 
-    void Update()
-    {
-        x = Input.GetAxisRaw("Horizontal");
-        y = Input.GetAxisRaw("Vertical");
+private Vector2 input;
 
-        if (x != 0)
-        {
-            move = new Vector2(x, 0);
-            if (x < 0)
-            {
-                sR.sprite = sprites[0];
-            }
-            else
-            {
-                sR.sprite = sprites[1];
-            }
-        }            
-        else if (y != 0)
-        {
-            move = new Vector2(0, y);
-            if (y < 0)
-            {
-                sR.sprite = sprites[2];
-            }
-            else
-            {
-                sR.sprite = sprites[3];
-            }
-        }
+void Update()
+{
+    input.x = Input.GetAxisRaw("Horizontal");
+    input.y = Input.GetAxisRaw("Vertical");
+
+    if (input.x != 0)
+    {
+        input.y = 0;
+
+        if (input.x < 0)
+            sR.sprite = sprites[0]; // left
         else
-        {
-            move = Vector2.zero;
-        }
-            
-
-        this.transform.Translate(move * speed * Time.deltaTime);
+            sR.sprite = sprites[1]; // right
     }
+    else if (input.y != 0)
+    {
+        if (input.y < 0)
+            sR.sprite = sprites[2]; // down
+        else
+            sR.sprite = sprites[3]; // up
+    }
+}
+
+void FixedUpdate()
+{
+    Vector2 move = input;
+
+    if (move.x != 0)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(body.position, new Vector2(move.x, 0), 0.1f, wallLayer);
+        if (hit.collider != null)
+        {
+            move.x = 0;
+        }
+    }
+
+    if (move.y != 0)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(body.position, new Vector2(0, move.y), 0.1f, wallLayer);
+        if (hit.collider != null)
+        {
+            move.y = 0;
+        }
+    }
+
+    body.linearVelocity = move.normalized * speed;
+}
 }
