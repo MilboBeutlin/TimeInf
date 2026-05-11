40b739856b4fa169fcdae417593e841fd96b27c5
MilboBeutlin <mionuss1@gmail.com>
2026-05-11 16:46:59 +0200

    enums fixed
    Das Enum Update von Leon gefixed

--- Assets/Scripts/AttacksKönig.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public enum AttacksDä
-{
-    NULL, DunklerSchnitt, Giftwurf, KronederVerdammnis, Dämonensphäre, ChaosLanze, Block, DunkleResonanz, KingsBreaker, EndloseDunkelheit,
-}
--- Assets/Scripts/AttacksSpieler.cs
+++ Assets/Scripts/AttacksSpieler.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 public enum Attacks
 {
-    NULL, Donnerschock, Blitzsturm, Freeze
+    NULL, Donnerschock, Blitzsturm, Freeze,
+    //AttackenKönig
+    DunklerSchnitt, Giftwurf, KronederVerdammnis, Dämonensphäre, ChaosLanze, Block, DunkleResonanz, KingsBreaker, EndloseDunkelheit,
 }
