7d042a4186e0db18762a342045755315a9bd1658
viviwes <vivienweser@gmx.de>
2026-05-17 20:54:47 +0200

    tür (wirklich)

--- Assets/Scripts/Game/Player_Game.cs
+++ Assets/Scripts/Game/Player_Game.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -68,5 +69,39 @@ void OnTriggerEnter2D(Collider2D other)
     {
          SceneManager.LoadScene("Fight");
     }
+    
+if (other.CompareTag("Tür"))
+        {
+
+    if (input.x != 0)
+            {
+                if (input.x > 0)
+                {
+                    body.position = new Vector2(body.position.x + 2, body.position.y);
+
+                }
+                else
+                {
+                    body.position = new Vector2(body.position.x - 2, body.position.y);
+
+                }
+            }
+
+if (input.y != 0)
+            {
+                if (input.y > 0)
+                {
+                    body.position = new Vector2(body.position.x, body.position.y + 2);
+                }
+
+                else
+                {
+                     body.position = new Vector2(body.position.x, body.position.y - 2);
+                }
+            }
+        }
+
+
+
 }
 }
