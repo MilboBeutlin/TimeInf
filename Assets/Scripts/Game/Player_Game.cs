f0ea9d5f39ce4dacfef3db2777ebd51ce4b4289e
DarthShifter <darthstein66@gmail.com>
2026-05-18 17:07:04 +0200

    tür?
    idk. it exploded once. dunno what to do. i just copied the code

--- Assets/Scripts/Game/Player_Game.cs
+++ Assets/Scripts/Game/Player_Game.cs
@@ -98,7 +98,36 @@ void OnTriggerEnter2D(Collider2D other)
                 break;
         }
         SceneManager.LoadScene("Fight");
-    }
+    }else if (other.CompareTag("Tür"))
+        {
+
+            if (input.x != 0)
+            {
+                if (input.x > 0)
+                {
+                    body.position = new Vector2(body.position.x + 2, body.position.y);
+
+                }
+                else
+                {
+                    body.position = new Vector2(body.position.x - 2, body.position.y);
+
+                }
+            }
+
+            if (input.y != 0)
+            {
+                if (input.y > 0)
+                {
+                    body.position = new Vector2(body.position.x, body.position.y + 2);
+                }
+
+                else
+                {
+                     body.position = new Vector2(body.position.x, body.position.y - 2);
+                }
+            }
+        }
 }
 
     private void EnemyLoading(int lp, int atk, int armor, int speed, int dk)
