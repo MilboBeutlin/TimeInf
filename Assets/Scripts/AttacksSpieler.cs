6b9d2710ba727b457e2fe9128a27ff49fb877d7a
leonreinecke08-sys <leon.reinecke08@gmail.com>
2026-05-22 16:00:01 +0200

    Attacken enum fertig

--- Assets/Scripts/AttacksSpieler.cs
+++ Assets/Scripts/AttacksSpieler.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public enum Attacks
 {
-    NULL, Donnerschock, Blitzsturm, Freeze,
+    //AttackenSpieler
+    Schutz, Hammer, Verstümmelung, Schlag, Giftdolch, Feuerball, Dämpfer, Vergeltung, Graben, LichtderHoffnung, Erlösungsschlag, Reinigung, Leidensstoß, Erleuchtung, Schwimmen, 
     //AttackenK�nig
     DunklerSchnitt, Giftwurf, KronederVerdammnis, Daemonensphaere, ChaosLanze, Block, DunkleResonanz, KingsBreaker, EndloseDunkelheit,
 }
