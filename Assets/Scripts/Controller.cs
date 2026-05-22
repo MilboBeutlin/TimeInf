d25ce193063827dd75468a4b8eaefecba08b3563
DarthShifter <darthstein66@gmail.com>
2026-05-22 16:43:53 +0200

    mvc getter&setter revised

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -21,33 +21,16 @@ public class Controller : MonoBehaviour
     }
 
     // player
-
-    public Attacks[] GetCurrentPlayerAttacks() {
-        return model.GetCurrentPlayerAttacks();
-    }
-
     public void SetCurrentPlayerAttacks(Attacks[] attacks) {
         model.SetCurrentPlayerAttacks(attacks);
-    }
-
-    public int[] GetCurrentPlayerStats() {
-        return model.GetCurrentPlayerStats();
     }
 
     public void SetCurrentPlayerStats(int[] stats) {
         model.SetCurrentPlayerStats(stats);
     }
 
-    public Statuseffekte[] GetCurrentPlayerEffects() {
-        return model.GetCurrentPlayerEffects();
-    }
-
     public void SetCurrentPlayerEffects(Statuseffekte[] effects) {
         model.SetCurrentPlayerEffects(effects);
-    }
-
-    public Items[] GetCurrentPlayerItems() {
-        return model.GetCurrentPlayerItems();
     }
 
     public void SetCurrentPlayerItems(Items[] items) {
@@ -55,35 +38,15 @@ public class Controller : MonoBehaviour
     }
 
     // opponent
-
-    public Gegner GetCurrentOponent() {
-        return model.GetCurrentOponent();
-    }
-
     public void SetCurrentOponent(Gegner gegner) {
         model.SetCurrentOponent(gegner);
     }
-
-    public Attacks[] GetCurrentOponnentAttacks() {
-        return model.GetCurrentOponnentAttacks();
-    }
-
     public void SetCurrentOponnentAttacks(Attacks[] attacks) {
         model.SetCurrentOponnentAttacks(attacks);
     }
-
-    public Statuseffekte[] GetCurrentOponentEffects() {
-        return model.GetCurrentOponentEffects();
-    }
-
     public void SetCurrentOponentEffects(Statuseffekte[] effects) {
         model.SetCurrentOponentEffects(effects);
     }
-
-    public int[] GetCurrentOponnentStats() {
-        return model.GetCurrentOponnentStats();
-    }
-
     public void SetCurrentOponnentStats(int[] stats) {
         model.SetCurrentOponnentStats(stats);
     }
--- Assets/Scripts/Model.cs
+++ Assets/Scripts/Model.cs
@@ -22,69 +22,29 @@ public class Model : MonoBehaviour
     public Attacks[] GetCurrentPlayerAttacks() {
         return DB.GetCurrentPlayerAttacks();
     }
-
-    public void SetCurrentPlayerAttacks(Attacks[] attacks) {
-        DB.SetCurrentPlayerAttacks(attacks);
-    }
-
     public int[] GetCurrentPlayerStats() {
         return DB.GetCurrentPlayerStats();
     }
-
-    public void SetCurrentPlayerStats(int[] stats) {
-        DB.SetCurrentPlayerStats(stats);
-    }
-
     public Statuseffekte[] GetCurrentPlayerEffects() {
         return DB.GetCurrentPlayerEffects();
     }
-
-    public void SetCurrentPlayerEffects(Statuseffekte[] effects) {
-        DB.SetCurrentPlayerEffects(effects);
-    }
-
     public Items[] GetCurrentPlayerItems() {
         return DB.GetCurrentPlayerItems();
     }
-
-    public void SetCurrentPlayerItems(Items[] items) {
-        DB.SetCurrentPlayerItems(items);
-    }
-
     // opponent
 
     public Gegner GetCurrentOponent() {
         return DB.GetCurrentOponent();
     }
-
-    public void SetCurrentOponent(Gegner gegner) {
-        DB.SetCurrentOponent(gegner);
-    }
-
     public Attacks[] GetCurrentOponnentAttacks() {
         return DB.GetCurrentOponnentAttacks();
     }
-
-    public void SetCurrentOponnentAttacks(Attacks[] attacks) {
-        DB.SetCurrentOponnentAttacks(attacks);
-    }
-
     public Statuseffekte[] GetCurrentOponentEffects() {
         return DB.GetCurrentOponentEffects();
     }
-
-    public void SetCurrentOponentEffects(Statuseffekte[] effects) {
-        DB.SetCurrentOponentEffects(effects);
-    }
-
     public int[] GetCurrentOponnentStats() {
         return DB.GetCurrentOponnentStats();
     }
-
-    public void SetCurrentOponnentStats(int[] stats) {
-        DB.SetCurrentOponnentStats(stats);
-    }
-    
 
     //idk what:
     public void UpdateFightViews()
