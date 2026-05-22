8ee9bca49ae2dad3bdf4a4f98a781632daeed63a
MilboBeutlin <mionuss1@gmail.com>
2026-05-22 16:01:23 +0200

    GTA 69
    WASSSS ROCKSTAR HAT EINFACH ALLES GEMACHT OMGGGGGGGGGGGGGG FNAF 4 RELEAST AM 30. FEBRUAR NÄCHSTES JAHR. AHHHHHHHHHH

--- Assets/Scripts/AttacksSpieler.cs
+++ Assets/Scripts/AttacksSpieler.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public enum Attacks
 {
+    NULL,
     //AttackenSpieler
     Schutz, Hammer, Verstümmelung, Schlag, Giftdolch, Feuerball, Dämpfer, Vergeltung, Graben, LichtderHoffnung, Erlösungsschlag, Reinigung, Leidensstoß, Erleuchtung, Schwimmen, 
     //AttackenK�nig
