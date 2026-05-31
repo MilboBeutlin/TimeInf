7e9d9ba66b77541f555cbe11245b159938e35d98
DarthShifter <darthstein66@gmail.com>
2026-05-31 16:35:47 +0200

    R3 finished
    R3 finsihed, just some small details need to be adjusted, but that will be done after other stuff is done + playerSprite adjusted

---
+++ Assets/Scripts/Game/DoorEnemie_Game.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DoorEnemie_Game : MonoBehaviour
+{
+    [SerializeField] private Transform moveTo;
+    [SerializeField] private GameObject TextObject;
+    [SerializeField] private Text text;
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private Transform player;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(transform.position != moveTo.position)
+        {
+            text.text = "You dare to exist?";
+            TextObject.SetActive(true);
+        }
+        else
+        {
+            text.text = "This is our last meeting bitch";
+            TextObject.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        TextObject.SetActive(false);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Debug.Log("hi bitch");
+        if (text.text == "You dare to exist?")
+        {
+            transform.position = moveTo.position;
+            transform.eulerAngles = new Vector3(0, 0, 180);
+        }
+        else if (text.text == "This is our last meeting bitch")
+        {
+            Instantiate(enemy, player.position, Quaternion.identity);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+}
