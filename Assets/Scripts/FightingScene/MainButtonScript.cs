00245536f681ae25df83c6d1b0a6172b1d09c639
DarthShifter <darthstein66@gmail.com>
2026-06-20 22:20:20 +0200

    graphic
    lights, text, new slider in fight, etc.

--- Assets/Scripts/FightingScene/MainButtonScript.cs
+++ Assets/Scripts/FightingScene/MainButtonScript.cs
@@ -2,7 +2,7 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using static System.Net.Mime.MediaTypeNames;
+//using static System.Net.Mime.MediaTypeNames;
 
 public class MainButtonScript : MonoBehaviour
 {
@@ -21,7 +21,7 @@ public class MainButtonScript : MonoBehaviour
     [SerializeField] private GameObject FleeButtons;
 
     [SerializeField] private Items ButtonType;
-    [SerializeField] private TMP_Text ItemButtonText;
+    [SerializeField] private Text ItemButtonText;
 
     private bool FirstAttackPage = true;
     
@@ -44,30 +44,30 @@ public class MainButtonScript : MonoBehaviour
         {
             if(GM.givePlayerAttack(7) != Attacks.NULL && FirstAttackPage == false)
             {
-                Attack6.GetComponent<TMP_Text>().text = "NEXT";
-                Attack1.GetComponent<TMP_Text>().text = GM.givePlayerAttack(5).ToString();
-                Attack2.GetComponent<TMP_Text>().text = GM.givePlayerAttack(6).ToString();
-                Attack3.GetComponent<TMP_Text>().text = GM.givePlayerAttack(7).ToString();
-                Attack4.GetComponent<TMP_Text>().text = GM.givePlayerAttack(8).ToString();
-                Attack5.GetComponent<TMP_Text>().text = GM.givePlayerAttack(9).ToString();
+                Attack6.GetComponent<Text>().text = "NEXT";
+                Attack1.GetComponent<Text>().text = GM.givePlayerAttack(5).ToString();
+                Attack2.GetComponent<Text>().text = GM.givePlayerAttack(6).ToString();
+                Attack3.GetComponent<Text>().text = GM.givePlayerAttack(7).ToString();
+                Attack4.GetComponent<Text>().text = GM.givePlayerAttack(8).ToString();
+                Attack5.GetComponent<Text>().text = GM.givePlayerAttack(9).ToString();
 
             } else if(GM.givePlayerAttack(7) != Attacks.NULL && FirstAttackPage == true)
             {
-                Attack6.GetComponent<TMP_Text>().text = "NEXT";
-                Attack1.GetComponent<TMP_Text>().text = GM.givePlayerAttack(0).ToString();
-                Attack2.GetComponent<TMP_Text>().text = GM.givePlayerAttack(1).ToString();
-                Attack3.GetComponent<TMP_Text>().text = GM.givePlayerAttack(2).ToString();
-                Attack4.GetComponent<TMP_Text>().text = GM.givePlayerAttack(3).ToString();
-                Attack5.GetComponent<TMP_Text>().text = GM.givePlayerAttack(4).ToString();
+                Attack6.GetComponent<Text>().text = "NEXT";
+                Attack1.GetComponent<Text>().text = GM.givePlayerAttack(0).ToString();
+                Attack2.GetComponent<Text>().text = GM.givePlayerAttack(1).ToString();
+                Attack3.GetComponent<Text>().text = GM.givePlayerAttack(2).ToString();
+                Attack4.GetComponent<Text>().text = GM.givePlayerAttack(3).ToString();
+                Attack5.GetComponent<Text>().text = GM.givePlayerAttack(4).ToString();
 
             } else 
             {
-                Attack1.GetComponent<TMP_Text>().text = GM.givePlayerAttack(0).ToString();
-                Attack2.GetComponent<TMP_Text>().text = GM.givePlayerAttack(1).ToString();
-                Attack3.GetComponent<TMP_Text>().text = GM.givePlayerAttack(2).ToString();
-                Attack4.GetComponent<TMP_Text>().text = GM.givePlayerAttack(3).ToString();
-                Attack5.GetComponent<TMP_Text>().text = GM.givePlayerAttack(4).ToString();
-                Attack6.GetComponent<TMP_Text>().text = GM.givePlayerAttack(5).ToString();
+                Attack1.GetComponent<Text>().text = GM.givePlayerAttack(0).ToString();
+                Attack2.GetComponent<Text>().text = GM.givePlayerAttack(1).ToString();
+                Attack3.GetComponent<Text>().text = GM.givePlayerAttack(2).ToString();
+                Attack4.GetComponent<Text>().text = GM.givePlayerAttack(3).ToString();
+                Attack5.GetComponent<Text>().text = GM.givePlayerAttack(4).ToString();
+                Attack6.GetComponent<Text>().text = GM.givePlayerAttack(5).ToString();
             }
             
         }
@@ -102,7 +102,7 @@ public class MainButtonScript : MonoBehaviour
         }
     }
 
-    // Für Unity Buttons, um Attacken auszuführen
+    // Fï¿½r Unity Buttons, um Attacken auszufï¿½hren
     public void DoAttack(int i)
     {
         if (i == 5 && GM.givePlayerAttack(7) != Attacks.NULL)
---
+++ Assets/Scripts/lights.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class lights : MonoBehaviour
+{
+    [SerializeField] private Light2D lightSource;
+    [SerializeField] private float startIntensity = 1f;
+    [SerializeField] private float variation = 0.05f;
+    [SerializeField] private float speed = 0.2f;
+
+    void Update()
+    {
+        float noise = Mathf.PerlinNoise(Time.time * speed, 0f);
+        lightSource.intensity = startIntensity + (noise - 0.5f) * 2f * variation;
+    }
+
+
+
+    private void OnBecameVisible()
+    {
+        lightSource.enabled = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        lightSource.enabled = false;
+    }
+}
