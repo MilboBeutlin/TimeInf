a4db1c6a68b26934cabfaec440a3b3ab73bed26f
DarthShifter <darthstein66@gmail.com>
2026-07-11 22:40:41 +0200

    buttonBlockade
    an sich fertig, muss nur in gegner aufgerufen werden

---
+++ Assets/Scripts/FightingScene/ButtonBlockade.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+/// This class is for a button blockade prefab that can be clicked a certain number of times before being destroyed
+public class ButtonBlockade : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    private Sprite[] damageSprites;
+    private int clicksRemaining;
+    private int spriteIndex;
+
+    //allows to create the prefab with a certain number of clicks needed and a set of sprites to display as damage is taken
+    public void Setup(int clicksNeeded, Sprite[] sprites)
+    {
+        clicksRemaining = clicksNeeded;
+        damageSprites = sprites;
+
+        image.sprite = damageSprites[0];
+    }
+
+    /// Handles the click event for the button blockade
+    public void Click()
+    {
+        clicksRemaining--;
+
+        if (clicksRemaining <= 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        spriteIndex = damageSprites.Length - clicksRemaining;
+        if (spriteIndex >= 0 && spriteIndex < damageSprites.Length)
+        {
+            image.sprite = damageSprites[spriteIndex];
+        }
+
+    }
+    /* how to create the blockade prefab and set it up in the scene:
+    
+        GameObject obj = Instantiate(blockadePrefab, playerButton);
+
+        obj.transform.localPosition = Vector3.zero;
+
+        obj.GetComponent<Blockade>().Setup(clicksNeeded, blockadeSprites);
+    */
+}
